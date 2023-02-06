a029e563142913daf32855cddfe584a129dcd30c
albertopiffer <albertopiffer96@gmail.com>
2023-02-06 17:10:26 +0100

    FEAT funzione avanti

--- js/app.js
+++ js/app.js
@@ -6,4 +6,25 @@ const slides = [
     './img/05.jpg',
     ]
 
-    
+const inputContainer = document.getElementById('container')
+
+for (let i=0; i<5; i++) {
+    inputContainer.innerHTML += '<img src="' + slides [i] + '" alt="img" id="img' + i + '">'
+    document.getElementById("img" + i).style.display = "none";
+    document.getElementById("img" + i).style.position = "relative";
+}
+
+let current = 0
+console.log (current)
+
+document.getElementById("img" + current).style.display = "block";
+
+inputContainer.innerHTML += '<button id=avanti>avanti</button>'
+const avanti = document.getElementById('avanti')
+
+avanti.addEventListener('click', function () {
+    document.getElementById("img" + current).style.display = "none";
+    document.getElementById("img" + ++current).style.display = "block";
+
+    console.log (current)
+})
