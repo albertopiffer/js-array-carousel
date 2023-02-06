82eb74063aa679483a7c92510a0e60497ba714b7
albertopiffer <albertopiffer96@gmail.com>
2023-02-06 17:58:25 +0100

    FEAT markup sistemato (esercizio completo)

--- js/app.js
+++ js/app.js
@@ -7,11 +7,12 @@ const slides = [
     ]
 
 const inputContainer = document.getElementById('container')
+document.getElementById("container").style.position = "relative";
+document.getElementById("container").style.width = "fit-content";
 
 for (let i=0; i<5; i++) {
     inputContainer.innerHTML += '<img src="' + slides [i] + '" alt="img" id="img' + i + '">'
     document.getElementById("img" + i).style.display = "none";
-    document.getElementById("img" + i).style.position = "relative";
 }
 
 let active = 0
@@ -19,15 +20,27 @@ console.log (active)
 
 document.getElementById("img" + active).style.display = "block";
 
-inputContainer.innerHTML += '<button id=indietro>indietro</button>'
-inputContainer.innerHTML += '<button id=avanti>avanti</button>'
+inputContainer.innerHTML += '<i id=indietro class="fa-solid fa-chevron-left"></i>'
+inputContainer.innerHTML += '<i id=avanti class="fa-solid fa-chevron-right"></i>'
+
+document.getElementById("avanti").style.position = "absolute";
+document.getElementById("avanti").style.color = "white";
+document.getElementById("avanti").style.top = "50%";
+document.getElementById("avanti").style.right = "20px";
+document.getElementById("avanti").style.transform = "translateY(-50%)";
+
+document.getElementById("indietro").style.position = "absolute";
+document.getElementById("indietro").style.color = "white";
+document.getElementById("indietro").style.top = "50%";
+document.getElementById("indietro").style.left = "20px";
+document.getElementById("indietro").style.transform = "translateY(-50%)";
 
 const indietro = document.getElementById('indietro')
 const avanti = document.getElementById('avanti')
 
 avanti.addEventListener('click', function () {
     document.getElementById("img" + active).style.display = "none"
-    if (active == 4) { active = -1 }
+    if (active == 4) { active = 3 }
     document.getElementById("img" + ++active).style.display = "block"
 
     console.log (active)
@@ -35,7 +48,7 @@ avanti.addEventListener('click', function () {
 
 indietro.addEventListener('click', function () {
     document.getElementById("img" + active).style.display = "none"
-    if (active == 0) { active = 5 }
+    if (active == 0) { active = 1 }
     document.getElementById("img" + --active).style.display = "block"
 
     console.log (active)
